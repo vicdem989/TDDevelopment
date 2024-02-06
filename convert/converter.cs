0a2b5f451b0dcb30a434093d4ac3fbe450d1a5d1
Vicdem989 <vicdemhoy@hotmail.com>
2024-02-06 11:24:47 +0100

    added more stuff :)

--- convert/converter.cs
+++ convert/converter.cs
@@ -1,12 +1,20 @@
 namespace converter;
 
 class Converter {
-    public static int Convert(double numberToConvert, string format) {
+    public static double Convert(double numberToConvert, string format) {
 
         /// Find a solution to figure out if format is mm, cm, m
-        /// 
+        /// Having converter class
+        /// mm : converter
+        /// cm : converter
+        /// m : converter
 
 
-        return 1;
+
+        if(format == "mm") {
+            return numberToConvert * 25.4;
+        }
+
+        return 
     }
 }
